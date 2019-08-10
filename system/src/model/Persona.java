68a4123602de243b132528dd73298ad50b6de0d2
Adrian Tomala <luisadriant11@hotmail.com>
2019-08-10 12:38:14 -0500

    creacion de clases-modelo

---
+++ system/src/model/Persona.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license * To change this template file, choose Tools | Templates
+ header, choose License Headers in Project Properties.
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ProyDS 3
+ */
+public class Persona {
+    
+}
