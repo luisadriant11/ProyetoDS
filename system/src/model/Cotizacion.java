b567a5abd0e32b07f8df1a8457dc3f0fc906abb9
Gabriela Cuadros <icuadros@espol.edu.ec>
2019-08-12 09:44:55 -0500

    implementacion de articulo y cotizacion

--- system/src/model/Cotizacion.java
+++ system/src/model/Cotizacion.java
@@ -5,7 +5,65 @@
  */
 package model;
 
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 
 public class Cotizacion extends Responsabilitty {
+    protected String codigo;
+    protected Date fecha;
+    protected float total;
+    protected LinkedList<Articulo> articulos;
+
+    public Cotizacion() {
+        this.codigo = "";
+        this.fecha = new Date();
+        this.total = 0;
+    }
+
+    public Cotizacion(Date fecha, float total) {
+        this.fecha = fecha;
+        this.total = total;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public float getTotal() {
+        calcularTotal();
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+
+    private void calcularTotal() {
+        Articulo art;
+        float cont=0;
+        for (Iterator<Articulo> it = articulos.iterator(); it.hasNext();) {
+            art = it.next();
+            cont+=art.getPrecio();
+        }
+        total=cont;       
+    }
     
+    @Override
+    public void setNextResponsability(){
+        
+    }
 }
