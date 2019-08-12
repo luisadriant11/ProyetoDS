2ce4ce738f1548817153999b90c8b7d900250ba7
Gabriela Cuadros <icuadros@espol.edu.ec>
2019-08-12 10:23:56 -0500

    implementacion de venta y local

--- system/src/model/Local.java
+++ system/src/model/Local.java
@@ -5,10 +5,38 @@
  */
 package model;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author ProyDS 3
  */
 public class Local {
+    protected String idLocal;
+    protected String direccion;
+    protected String telefono;
+    protected LinkedList<Articulo> articulos;
+    protected boolean internet;
+    protected LinkedList<Empleado> empleados;
+    protected Gerente gerente;
+    protected Administrador administrador;
+
+    public Local(String idLocal, String direccion, String telefono, LinkedList<Articulo> articulos, boolean internet, LinkedList<Empleado> empleados, Gerente gerente, Administrador administrador) {
+        this.idLocal = idLocal;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.articulos = articulos;
+        this.internet = internet;
+        this.empleados = empleados;
+        this.gerente = gerente;
+        this.administrador = administrador;
+    }
     
+    public void emitirDocumento(Transaccion t){
+        System.out.println("Documento emitido");
+    }
+    
+    public boolean verificarConexion(){
+        return internet;
+    }
 }
--- system/src/model/Venta.java
+++ system/src/model/Venta.java
@@ -5,7 +5,89 @@
  */
 package model;
 
+import java.util.Date;
+import java.util.LinkedList;
+
 
 public class Venta extends Responsabilitty {
+    protected String codigo;
+    protected Date fecha;
+    protected LinkedList<Articulo> productosVendidos;
+    protected float subtotal;
+    protected float iva;
+    protected float total;
+    protected Pago formaPago;
+
+    public Venta(LinkedList<Articulo> productosVendidos,Pago formaPago) {
+        this.codigo = "";
+        this.fecha = new Date();
+        this.productosVendidos = productosVendidos;
+        this.subtotal = 0;
+        this.iva = 0;
+        this.total = 0;
+        this.formaPago = formaPago;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public LinkedList<Articulo> getProductosVendidos() {
+        return productosVendidos;
+    }
+
+    public void setProductosVendidos(LinkedList<Articulo> productosVendidos) {
+        this.productosVendidos = productosVendidos;
+    }
+
+    public float getSubtotal() {
+        productosVendidos.forEach((i) -> {
+            subtotal+=i.precio;
+        });
+        return subtotal;
+    }
+
+
+    public float getIva() {
+        iva=(float) (subtotal*0.12);
+        return iva;
+    }
+
+
+
+    public float getTotal() {
+        total=getSubtotal()+getIva();
+        return total;
+    }
+
+
+    public Pago getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(Pago formaPago) {
+        this.formaPago = formaPago;
+    }
+    
+    
+    
+    
+    
+    @Override
+    public void setNextResponsability() {
+        super.setNextResponsability(); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
